2eb998937bf7456d88ab4afbf7cd2af1f05c8612
Shanika Wijerathna <shanikacj@gmail.com>
2016-10-19 20:05:04 +1300

    Added map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from "./app.component";
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 import {HttpModule} from "@angular/http";
+import { AgmCoreModule } from 'angular2-google-maps/core';
 import {Home} from "./home/home.component";
 import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 import {SearchBar} from "./search-bar/search-bar.component";
@@ -13,7 +14,8 @@ import {MapPanel} from "./map-panel/map-panel.component";
 
 @NgModule({
   declarations: [AppComponent, Header, SearchBar, MapPanel, Home],
-  imports     : [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig)],
+  imports     : [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig),
+                  AgmCoreModule.forRoot({apiKey: 'AIzaSyALYSPFzg_jpcc6xMgTAh3pr8wHxHzpmr0'})],
   providers   : [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap   : [AppComponent]
 })
--- src/app/map-panel/map-panel.component.ts
+++ src/app/map-panel/map-panel.component.ts
@@ -1,16 +1,20 @@
-import {Component, HostListener} from '@angular/core';
+import {Component, HostListener, ViewEncapsulation} from '@angular/core';
 
 const HEADER_HEIGHT:number = 126;
 
 @Component({
   selector: 'map-panel',
   styleUrls: ['./map-panel.component.scss'],
-  templateUrl: './map-panel.component.html'
+  templateUrl: './map-panel.component.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class MapPanel {
 
 
   mapHeight : number = window.innerHeight - HEADER_HEIGHT;
+
+  lat: number = -36.739612;
+  lng: number = 174.738668;
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
